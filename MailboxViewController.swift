625dc3f7d680641a152336fed7ee8098193b55dc
Marc Haumann <marchaumann@gmail.com>
2016-02-20 14:12:06 -0800

    add inbox view and scrollview, with menu behind

---
+++ MailboxViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MailboxViewController.swift
+//  Mailbox
+//
+//  Created by Marc Haumann on 2/20/16.
+//  Copyright © 2016 Marc Haumann. All rights reserved.
+//
+
+import UIKit
+
+class MailboxViewController: UIViewController {
+
+    @IBOutlet weak var inboxView: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.contentSize = CGSize(width: 320, height: 1202)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
