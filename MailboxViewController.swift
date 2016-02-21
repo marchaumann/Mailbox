0f710ae76e2170f6d9c6ecbf5f5a98c7f0f7cfaa
Marc Haumann <marchaumann@gmail.com>
2016-02-20 22:59:30 -0800

    message collapses on delete, positioning tweaks

--- MailboxViewController.swift
+++ MailboxViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 
 class MailboxViewController: UIViewController {
 
+    @IBOutlet weak var feedView: UIImageView!
     @IBOutlet weak var iconArchiveView: UIImageView!
     @IBOutlet weak var iconLaterView: UIImageView!
     @IBOutlet weak var messageView: UIImageView!
     @IBOutlet weak var messageContainer: UIView!
     @IBOutlet weak var inboxView: UIView!
     @IBOutlet weak var scrollView: UIScrollView!
-    var initialAlpha: CGFloat! = 0
+    var messageState: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,34 +32,72 @@ class MailboxViewController: UIViewController {
     
     @IBAction func messageDidPan(sender: UIPanGestureRecognizer) {
         let translation = sender.translationInView(view)
-        self.messageView.frame.origin.x = translation.x
+        messageView.frame.origin.x = translation.x
         
         if sender.state == UIGestureRecognizerState.Began {
             
         } else if sender.state == UIGestureRecognizerState.Changed {
-            iconArchiveView.alpha = convertValue(translation.x, r1Min: 0, r1Max: 60, r2Min: initialAlpha, r2Max: 1)
-            iconLaterView.alpha = convertValue(translation.x, r1Min: 0, r1Max: -60, r2Min: initialAlpha, r2Max: 1)
-            if 60 ... 250 ~= translation.x {
-                self.messageContainer.backgroundColor = UIColor.greenColor()
+            iconArchiveView.alpha = convertValue(translation.x, r1Min: 0, r1Max: 60, r2Min: 0, r2Max: 1)
+            iconLaterView.alpha = convertValue(translation.x, r1Min: 0, r1Max: -60, r2Min: 0, r2Max: 1)
+            if 60 ... 240 ~= translation.x {
+                messageContainer.backgroundColor = UIColor.greenColor()
                 iconArchiveView.image = UIImage(named: "archive_icon")
+                iconArchiveView.frame.origin.x = translation.x - 40
+                messageState = "archive"
             }
-            else if -250 ... -60 ~= translation.x {
-                self.messageContainer.backgroundColor = UIColor.yellowColor()
+            else if -240 ... -60 ~= translation.x {
+                messageContainer.backgroundColor = UIColor.yellowColor()
                 iconLaterView.image = UIImage(named: "later_icon")
+                iconLaterView.frame.origin.x = translation.x + 340
+                messageState = "later"
             }
-            else if translation.x > 250 {
-                self.messageContainer.backgroundColor = UIColor.redColor()
+            else if translation.x > 240 {
+                messageContainer.backgroundColor = UIColor.redColor()
                 iconArchiveView.image = UIImage(named: "delete_icon")
+                iconArchiveView.frame.origin.x = translation.x - 40
+                messageState = "delete"
             }
-            else if translation.x < -250 {
-                self.messageContainer.backgroundColor = UIColor.brownColor()
+            else if translation.x < -240 {
+                messageContainer.backgroundColor = UIColor.brownColor()
                 iconLaterView.image = UIImage(named: "list_icon")
+                iconLaterView.frame.origin.x = translation.x + 340
+                messageState = "list"
             }
             else {
-                self.messageContainer.backgroundColor = UIColor.grayColor()
+                messageContainer.backgroundColor = UIColor.grayColor()
+                messageState = "release"
             }
         } else if sender.state == UIGestureRecognizerState.Ended {
-            
+            switch messageState {
+                case "archive": UIView.animateWithDuration(0.3, delay: 0, options: [], animations: { () -> Void in
+                        self.messageView.frame.origin.x = 320
+                        self.iconArchiveView.frame.origin.x = 320-25-19
+                    }, completion: nil)
+                case "later": UIView.animateWithDuration(0.3, delay: 0, options: [], animations: { () -> Void in
+                    self.messageView.frame.origin.x = -320
+                    }, completion: {(Bool) -> Void in
+                        UIView.animateWithDuration(0.4, delay: 0, options: [], animations: { () -> Void in
+                                self.messageContainer.transform = CGAffineTransformMakeScale(1, 0)
+                            }, completion: nil)
+                })
+                case "delete": UIView.animateWithDuration(0.2, delay: 0, options: [], animations: { () -> Void in
+                    self.messageView.frame.origin.x = 320
+                    self.iconArchiveView.frame.origin.x = 320-25-19
+                    }, completion: {(Bool) -> Void in
+                        UIView.animateWithDuration(0.4, delay: 0, options: [], animations: { () -> Void in
+                            self.feedView.transform = CGAffineTransformMakeTranslation(0, -84)
+                            self.messageContainer.transform = CGAffineTransformMakeTranslation(0, -42)
+                            self.messageContainer.transform = CGAffineTransformScale(self.messageContainer.transform, 1, 0.00000000001)
+
+                            }, completion: nil)
+                })
+            case "list": UIView.animateWithDuration(0.2, delay: 0, options: [], animations: { () -> Void in
+                self.messageView.frame.origin.x = -320
+                }, completion: nil)
+                default: UIView.animateWithDuration(0.3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: [], animations: { () -> Void in
+                    self.messageView.frame.origin.x = 0
+                    }, completion: nil)
+            }
         }
         
     }
